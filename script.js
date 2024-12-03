17425f5371ec1317a9b24374049841393db0fbe5
Leon <l.cohen@web.de>
2024-12-03 13:57:47 +0100

    adjusted &/or transferred code from testing files

--- script.js
+++ script.js
@@ -178,6 +178,10 @@ canvas.height = window.innerHeight;
 
 const gravity = 1.5;
 
+//////////////
+////PLAYER////////////////////////////////////////////////////
+//////////////
+
 class Player {
   constructor() {
     this.position = {
@@ -209,7 +213,29 @@ class Player {
   }
 }
 
+//////////////
+//PLATFORMS///////////////////////////////////////////////////
+//////////////
+
+class Platform {
+  constructor() {
+    this.position = {
+      x: 200,
+      y: 100,
+    };
+    this.width = 200;
+    this.height = 20;
+  }
+
+  draw() {
+    c.fillStyle = "gray";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
 const player = new Player();
+const platform = new Platform();
+
 const keys = {
   right: {
     pressed: false,
@@ -219,18 +245,40 @@ const keys = {
   },
 };
 
+/////////////////////////
+///ENVIRONMENT/PHYSICS////////////////////////////////
+/////////////////////////
+
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
   player.update();
+  platform.draw();
 
   if (keys.right.pressed) {
     player.velocity.x = 5;
   } else if (keys.left.pressed) {
     player.velocity.x = -5;
   } else player.velocity.x = 0;
+
+  //PLATFORM COLLISION DETCTION
+
+  if (
+    player.position.y + player.height <= platform.position.y &&
+    player.position.y + player.height + player.velocity.y >=
+      platform.position.y &&
+    player.position.x + player.width >= platform.position.x &&
+    player.position.x <= platform.position.x + platform.width
+  ) {
+    player.velocity.y = 0;
+  }
 }
+
 animate();
+
+//////////////
+///CONTROLS/////////////////////////////////////////////////
+//////////////
 
 window.addEventListener("keydown", ({ keyCode }) => {
   switch (keyCode) {
@@ -277,5 +325,3 @@ window.addEventListener("keyup", ({ keyCode }) => {
       break;
   }
 });
-
-const test = "hallo";
